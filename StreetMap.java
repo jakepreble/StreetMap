2da5c6bce0fbc305ae462051980fb3f501308c48
jakepreble <jakepreble3@gmail.com>
2023-06-12 00:43:31 -0400

    fefeda

--- StreetMap.java
+++ StreetMap.java
@@ -16,7 +16,7 @@ public class StreetMap extends GraphAdjList<Integer, Integer, Double> {
     // 1. a double called 'distance' (shortest known distance from start)
     // 2. an integer called 'previous' (previous vertex in path)
     //YOUR CODE HERE:
-    private VertexInfo vertexInfo;
+    private static record VertexInfo(double distance, int previous) {}
     
 
     //if there is no previous vertex, 'previous' is set to -1
@@ -86,9 +86,9 @@ public class StreetMap extends GraphAdjList<Integer, Integer, Double> {
          //double distance = info.get(nearestKey).getDistance();
          double minDistance = Double.POSITIVE_INFINITY;
          for(int i = 0; i < unvisited.size(); i++){
-            if(info.get(i).getDistance() < minDistance){
+            if(info.get(i).distance() < minDistance){
                 nearestKey = i;
-                minDistance = info.get(nearestKey).getDistance();
+                minDistance = info.get(nearestKey).distance();
             }
          } 
          return nearestKey;
@@ -101,11 +101,11 @@ public class StreetMap extends GraphAdjList<Integer, Integer, Double> {
         //YOUR CODE HERE:
         getVertex(point).setColor(color);
         
-        while(info.get(point).getPrevious() != NO_PREVIOUS){//){ {
+        while(info.get(point).previous() != NO_PREVIOUS){//){ {
             //set 'point' to the vertex before it
             //then set the color of vertex 'point' to 'color'
             //YOUR CODE HERE:
-            point = info.get(point).getPrevious();
+            point = info.get(point).previous();
             getVertex(point).setColor(color);
             
         }
@@ -126,13 +126,13 @@ public void shortestPath(int start, int end, Color color) {
             double distance = (i == start) ? 0 : Double.POSITIVE_INFINITY;
             info.put(i, new VertexInfo(distance, NO_PREVIOUS));
         }
-         System.out.println(" inside shortest path outside for loop to add indices" );
+         //System.out.println(" inside shortest path outside for loop to add indices" );
         while(true) {
             int current = nearestUnvisitedVertex(info, unvisited);
 
             //if the distance value of 'current' is infinity, quit the method with a 'return' statement
             //SOLUTION:
-            if(info.get(current).getDistance() == Double.POSITIVE_INFINITY) return;
+            if(info.get(current).distance() == Double.POSITIVE_INFINITY) return;
 
             //if the current vertex is 'end', use 'pathFromStart' to draw a path
             //then quit the method with a 'return' statement
@@ -141,12 +141,12 @@ public void shortestPath(int start, int end, Color color) {
                 pathFromStart(info, current, color);
                 return;
             }
-             System.out.println(" inside while true, didn't return yet");
+             //System.out.println(" inside while true, didn't return yet");
             for(Edge<Integer, Double> edge : this.getAdjacencyList(current)) {
                 //get the destination of the edge and assign it to a variable called 'vertex'
                 //if 'vertex' has already been visited, go to the next edge with a 'continue' statement
                 //SOLUTION:
-                System.out.println("inside foe edge loop");
+                //System.out.println("inside foe edge loop");
                 int vertex = edge.getTo();
                 if(!unvisited.contains(vertex)) continue;
 
@@ -155,8 +155,11 @@ public void shortestPath(int start, int end, Color color) {
                 //  create a new VertexInfo object associated with 'vertex'; the 'distance' field
                 //  should be the alternative distance and the 'previous' field should be 'current'
                 //SOLUTION:
-                double alternative = info.get(current).getDistance() + edge.getEdgeData();
-                if(alternative < info.get(vertex).getDistance()) {
+
+                double alternative = info.get(current).distance() + edge.getEdgeData();
+                System.out.println("Original:  " + info.get(vertex).distance());
+                System.out.println("alt: " + alternative);
+                if(alternative < info.get(vertex).distance()) {
                     info.put(vertex, new VertexInfo(alternative, current));
                 }
             }
